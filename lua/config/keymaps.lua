10a3caa92fef2427233150a2c8c550cbca10c4bb
jeff <jbonesteel130@gmail.com>
2023-12-14 21:00:38 -0600

    Keymaps: added pnae navigaion and window mangament commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,19 @@
 local keymap = vim.keymap
 
+local opts = { noremap = true, silent = true }
+
 -- Directory Navagaiton
 keymap.set('n', '<leader>m', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
 keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Pane Navigation
+keymap.set('n', '<C-h>', '<C-w>h', opts) --Left
+keymap.set('n', '<C-j>', '<C-w>j', opts) --Down 
+keymap.set('n', '<C-k>', '<C-w>k', opts) --Up 
+keymap.set('n', '<C-l>', '<C-w>l', opts) --Right
+
+-- Window Managment
+keymap.set('n', '<leader>sv', ':vsplit<CR>', opts) --Split Vertically
+keymap.set('n', '<leader>sh', ':split<CR>', opts) -- Slpit Horizontally
+keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>', opts) --Toggle Minimize
+
