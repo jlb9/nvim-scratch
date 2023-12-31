e9ea49f77613654d09dd614ab2767a6943b8e4b3
jeff <jbonesteel130@gmail.com>
2023-12-30 22:24:55 -0600

    delete all older files that were  replace.

--- lua/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'numToStr/Comment.nvim',
-  opts = {
-    -- add any optrions here
-  },
-  lazy = false,
-}
--- lua/plugins/onedark.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"navarasu/onedark.nvim",
-	lazy = false,
-	priority = 999,
-	config = function()
-		vim.cmd.colorscheme 'onedark'
-	end,
-}
